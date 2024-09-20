2fd09f1cfce20d61800b8ada4f447cf939f0a495
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-20 11:21:57 +0530

    feat(auth): implement Redux store and persistant  for user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./Pages/HomePage/HomePage";
-import ProtectedRoute from "./Components/ProtectedRoute";
+import ProtectedRoute from './Components/ProtectedRoute';
 import CommonPage from "./Pages/AuthPage";
 import UserProfile from './Pages/Profile/UserProfile';
-import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
     <>
-    <AuthProvider>
+ 
       <Router>
         <Routes>
           <Route path="/register" element={<CommonPage page="register" />} />
           <Route path="/OTP-Verification" element={<CommonPage page="otp" />}  />
           <Route path="/login" element={<CommonPage page="login" />} />
-          <Route path="/" element={<ProtectedRoute />}>
+          <Route element={<ProtectedRoute />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/profile" element={<UserProfile />} />
-          </Route>
-         
+        </Route>
          
         </Routes>
       </Router>
-      </AuthProvider>
+    
     </>
   );
 }
