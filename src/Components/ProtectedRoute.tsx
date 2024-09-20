7a806de5bcb9f964d627b05caacbb5de8dbeb3a6
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-20 11:15:29 +0530

    fix(protected-Route) : fixing the ibug in protected route using  redux store

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,29 +1,23 @@
-
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store/store'; 
 import Navbar from './Navbar';
-import { useAuth } from '../contexts/AuthContext';
-
 
 const ProtectedRoute: React.FC = () => {
-    console.log("verifyong");
-    
-    const { isAuthenticated } = useAuth();
+    const { user } = useSelector((state: RootState) => state.auth);
 
-    console.log("Authenticated:", isAuthenticated);
+    const isAuthenticated = !!user; 
+
     if (!isAuthenticated) {
-
-        console.log("Redirecting to login");
         return <Navigate to="/login" replace />;
     }
 
     return (
         <div className="flex h-screen">
-            <div className="w-16 bg-gray-100">
-                <Navbar />
-            </div>
-            <div className="flex-1 bg-white">
-                <Outlet />
+            <Navbar /> 
+            <div className="flex-1 p-4">
+                <Outlet /> 
             </div>
         </div>
     );
