fc4175093bc96e021f4685b86d247e4215abd132
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-27 23:21:06 +0530

    feat: addnew component folder for user seperatley and added the navbar and protected route

---
+++ src/Components/User/ProtectedRoute.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store/store'; 
+import Navbar from './Navbar';
+
+const ProtectedRoute: React.FC = () => {
+    const { user } = useSelector((state: RootState) => state.auth);
+
+    const isAuthenticated = !!user; 
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return (
+        <div className="flex h-screen">
+            <Navbar /> 
+            <div className="flex-1 p-5 overflow-auto">
+                <Outlet /> 
+            </div>
+        </div>
+    );
+};
+
+export default ProtectedRoute;
