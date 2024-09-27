a50ee4fe227cccac1fc5281d1072a721d4294460
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-27 23:16:59 +0530

    feat:add the user profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./Pages/HomePage/HomePage";
-import ProtectedRoute from './Components/ProtectedRoute';
+import { useSelector } from 'react-redux';
+import ProtectedRoute from './Components/User/ProtectedRoute';
 import CommonPage from "./Pages/AuthPage";
 import UserProfile from './Pages/Profile/UserProfile';
-
+import { RootState } from './redux/store/store';
+import './theme.css'
+import LoginPage from './Pages/admin/LoginPage';
+import AdminProtectedRoute from './Components/Admin/AdminProtectedRoute';
+import AdminDashboard from './Pages/admin/AdminDashBoard';
 
 function App() {
+  const { darkMode } = useSelector((state: RootState) => state.theme);
+
   return (
-    <>
- 
+    <div className={darkMode ? 'dark bg-black text-white' : 'bg-white text-black'}>
       <Router>
         <Routes>
+          {/* Public routes */}
           <Route path="/register" element={<CommonPage page="register" />} />
-          <Route path="/OTP-Verification" element={<CommonPage page="otp" />}  />
+          <Route path="/OTP-Verification" element={<CommonPage page="otp" />} />
           <Route path="/login" element={<CommonPage page="login" />} />
+
+          {/* Protected routes */}
           <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/profile" element={<UserProfile />} />
-        </Route>
-         
+            <Route path="/" element={<HomePage />} />
+            <Route path="/profile" element={<UserProfile />} />
+          </Route>
+
+          {/* Admin routes */}
+          <Route element={<AdminProtectedRoute/>}>
+            <Route path='/adminDashBord' element={<AdminDashboard/>}/>
+          </Route>
+          
+          {/* Admin login route */}
+          <Route path="/adminLogin" element={<LoginPage/>} />
         </Routes>
       </Router>
-    
-    </>
+    </div>
   );
 }
 
