f7db0941b9cc2342a6545ac7347829a5651fb22f
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-27 23:28:02 +0530

    feat:  add new  admin and theme in sthe storage and  created the  authreducer ,themereducer to manage

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -4,19 +4,22 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; 
 import { PersistConfig } from 'redux-persist/es/types';
 import authReducer from '../slices/authSlice'; 
-
+import themeReducer from '../slices/themeSlice';
+import adminReducer from '../slices/adminSlice'
 export type RootState = ReturnType<typeof rootReducer>;
 
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  theme:themeReducer,
+  admin:adminReducer
 });
 
 
 const persistConfig: PersistConfig<RootState> = {
   key: 'root',
   storage,
-  whitelist: ['auth'], 
+  whitelist: ['auth','theme','admin'], 
 };
 
 
