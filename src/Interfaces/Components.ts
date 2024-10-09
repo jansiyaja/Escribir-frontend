fb790af11f0774906acdba1fde8af2efe185d19c
Jansiya jahan <jansiyajahan8@gmail.com>
2024-10-09 15:14:14 +0530

    feat(interface):add new interface for blog post card components

--- src/Interfaces/Components.ts
+++ src/Interfaces/Components.ts
@@ -24,3 +24,68 @@
     type: "success" | "error"; 
   }
   
+ export  interface FormFieldProps {
+    label: string;
+    name: string;
+    value: string;
+    editMode: boolean;
+    type?: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+    darkMode: boolean;
+    isTextArea?: boolean;
+  }
+  export interface BlogPreviewProps {
+    heading: string;
+    image: string | null;
+    tag: string;
+    content: string;
+    onClose: () => void; 
+  }
+
+  export interface AvatarProps {
+    src?: string;           
+    fallback?: string;     
+    size?: 'sm' | 'md' | 'lg'; 
+  }
+
+  ////////////////////////////////////////////////////////////////////////
+  interface Author {
+    image: string;
+    username: string;
+  }
+
+  export interface BlogPostCardProps {
+    _id: string;
+    heading: string;
+    tag: string;
+    coverImageUrl: string;
+    createdAt: string;
+    author_id : Author
+      
+    content?:string
+  }
+
+
+
+  /////////////////////////////////////////////////////////////////////////////////
+
+export interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+
+  confirmMessage?: string;
+  onConfirm?: () => void;
+  children?: React.ReactNode; 
+}
+  
+
+export interface CreateTagProps {
+  onTagCreatedOrUpdated: () => void;  
+  tagToEdit: { _id: string; name: string } | null; 
+  isEditMode: boolean;
+}
+export interface Tag {
+  _id: string;
+  name: string;
+  createdAt: string;
+}
