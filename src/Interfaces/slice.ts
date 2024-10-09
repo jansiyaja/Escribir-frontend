4880f39113709ab91e49fbade75f86e096fb9d92
Jansiya jahan <jansiyajahan8@gmail.com>
2024-10-09 15:17:13 +0530

    fix(user) : fix the user interface

--- src/Interfaces/slice.ts
+++ src/Interfaces/slice.ts
@@ -1,6 +1,46 @@
+
+
+export interface User {
+    _id: string;
+    username: string;
+    email: string;
+    role: string;
+    dob:string;
+    linkedIn:string;
+    github:string;
+    portfolio:string;
+    location:string;
+    bio:string;
+    phone?:string;
+    image?:string
+  }
+  
+  
+ export interface Admin {
+    id: string;
+    username: string;
+    email: string;
+    role: string;
+   
+  }
+
 export interface AuthState{
-    user: any;
+    user:User| null;
     accessToken:string;
     refreshToken:string;
 
     }
+
+    export interface AdminAuthState{
+        admin :Admin|null;
+        accessToken:string;
+        refreshToken:string;
+    
+        }
+       
+ 
+ export interface ThemeState{
+    darkMode: boolean;
+   
+    
+}
